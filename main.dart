243b7b73e3ed8cca79e1ed5df2fbb7ea9a564da2
Ismael <alquemist.dev@gmail.com>
2023-04-01 12:16:04 -0300

    Adicionando funcionalidades básicas

--- main.dart
+++ main.dart
@@ -4,8 +4,8 @@ import 'dart:math';
 class Expressao {
   String dado;
   List ordem = [];
-  num resultado = 0.0;
-  static Map<String, Function> regras = {
+  var resultado;
+  static Map<String, Function> regras_operadores = {
     '^': (int index, List formula) {
       num result = 0;
       result = pow(
@@ -59,15 +59,26 @@ class Expressao {
       }
     },
   };
-
-  static List stringToList({
-    required String valor,
-  }) {
+  static List<Map<String, String>> regras_ordem = [
+    {
+      'inicio': '(',
+      'fim': ')',
+    },
+    {
+      'inicio': '[',
+      'fim': ']',
+    },
+    {
+      'inicio': '{',
+      'fim': '}',
+    },
+  ];
+  static List stringToList({required String valor}) {
     List resultado = [];
     bool unirNum = false;
     bool unirLetras = false;
     Set num = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'};
-    Set letras = {'s', 'e', 'n', 'c', 'o', 't', 'g'};
+    Set letras = {'s', 'e', 'n', 'c', 'o', 't', 'a'};
     for (int i = 0; i < valor.length; i++) {
       if (valor[i] == ' ') {
       } else if (num.contains(valor[i])) {
@@ -97,38 +108,68 @@ class Expressao {
 
   Expressao({required this.dado}) {
     this.ordem = stringToList(valor: this.dado);
+    print(this.ordem);
   }
 
   calcularFormula({required List formula}) {
-    for (String op in regras.keys) {
+    for (String op in regras_operadores.keys) {
       int i = 0;
       int tamanho = formula.length;
       while (true) {
         if (i >= tamanho) break;
         if (formula[i] == op) {
-          regras[op]!(i, formula);
+          regras_operadores[op]!(i, formula);
           i = 0;
           tamanho = formula.length;
-          print(
-              '= ${formula.reduce((value, element) => value += ' ' + element)}');
         }
         i++;
       }
     }
-    return formula[0];
+    return formula.last;
+  }
+
+  segmentar({required List exp}) {
+    List total = exp;
+    List parte = [];
+    bool agrupar = false;
+    List aux = exp;
+    for (var o in regras_ordem) {
+      aux = total;
+      total = [];
+      for (var e in aux) {
+        if (o['inicio'] == e) {
+          agrupar = true;
+        } else if (o['fim'] == e) {
+          agrupar = false;
+          total.add(calcularFormula(formula: parte));
+          parte = [];
+        } else {
+          if (agrupar) {
+            parte.add(e);
+          } else {
+            total.add(e);
+          }
+        }
+      }
+      print('= ${aux.reduce((value, element) => value += element)}');
+    }
+    print('= ${total.reduce((value, element) => value += element)}');
+    return calcularFormula(formula: total);
   }
 
   void calcular() {
-    var result = calcularFormula(formula: ordem);
+    this.resultado = segmentar(exp: ordem);
   }
 }
 
 void main(List<String> args) {
+  List<String> exemplos = ["{(2+5)*[(3+4)+(2^2)]}*3", "[2^(0.5)]", "4 * (1/2)"];
   String entrada = '';
   do {
     entrada = stdin.readLineSync().toString();
     if (entrada == '') break;
     Expressao expressao = Expressao(dado: entrada);
     expressao.calcular();
+    print('= ${expressao.resultado}');
   } while (true);
 }
