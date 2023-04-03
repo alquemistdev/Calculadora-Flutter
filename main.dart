71a2f7b515e8372b8a36fc6857b48d3b72ac6f3c
Ismael <alquemist.dev@gmail.com>
2023-04-02 23:29:47 -0300

    Iniciando documentação

--- main.dart
+++ main.dart
@@ -1,3 +1,51 @@
+/*
+    ►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►  English ◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄
+    
+    Hello, I'm glad you went into my repository and looked at this project ;)
+    Here, we have an expression class that receives a string, processes it and calculates the
+    end result, like those cell phone calculators. The documentation for this project
+    would be in English.
+
+    Functionalities:
+      1 - Calculation with basic operators: + - (add), - - (subtract), * - (multiply), / - (divide);
+      2 - Calculates functions: sin(), cos(), tan();
+      3 - Processes data of type int and double: 1 and 0.99;
+      4 - The size of the expression has no determined limit;
+
+    However, this code has some noted limitations:
+      1 - The calculator does not accept two signs, example: ++, --, //, .., ^^, (()), [[]] and {{}};
+      2 - The conversion functions from radians to degrees and vice versa are inaccurate;
+      3 - The entry of arguments by "main(List<String> args)" ends up removing the exponentiation operator "^"
+      leaving the place empty;
+      4 - It does not have an input treatment of incorrect user values;
+
+    Therefore, you are free to evolve and optimize the code, correcting bugs and broken logic.
+
+
+
+    ►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►►  Portuguese ◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄◄
+    
+    Olá, fico feliz que você tenha entrado no meu repositório e olhado este projeto ;)
+    Aqui, temos uma classe expressão que recebe uma string, faz o tratamento e calcula o
+    resultado fim, como se fosse aquelas calculadoras de celular. A documentação deste projeto
+    ficara em inglês.
+
+    Funcionalidades:
+      1 - Calculo com operadores básicos: + - (somar), - - (subtrair), * - (multiplicar), / - (dividir);
+      2 - Faz calculo de funções: sin(), cos(), tan();
+      3 - Processa dados de tipo int e double: 1 e 0.99;
+      4 - O tamanho da expressão não tem limite determinado;
+
+    Entretando, este codigo possui algumas limitações observadas:
+      1 - A calculadora não aceita sinais duplos, exemplo: ++, --, //, .., ^^, (()), [[]] e {{}};
+      2 - O as funções de conversão de radianos para graus e vice-versa são imprecissas;
+      3 - A entrada de argumentos por "main(List<String> args)" acaba retirando o operador de exponenciação "^"
+      deixando o lugar vazio;
+      4 - Não possui um tratamento de entrada de valores incorretos do usuario;
+
+    Por isso, vocês tem liberdade para evoluir e otimizar o codigo, corrigindo bugs e lógicas quebradas.
+*/
+
 import 'dart:io';
 import 'dart:math';
 
@@ -7,6 +55,20 @@ class Expressao {
   var resultado;
 
   static double rad = 180 / 3.14;
+  static List<Map<String, String>> regras_ordem = [
+    {
+      'inicio': '(',
+      'fim': ')',
+    },
+    {
+      'inicio': '[',
+      'fim': ']',
+    },
+    {
+      'inicio': '{',
+      'fim': '}',
+    },
+  ];
   static Map<String, Function> regras_operadores = {
     'sin': (int index, List formula) {
       double result = 0;
@@ -82,20 +144,6 @@ class Expressao {
       }
     },
   };
-  static List<Map<String, String>> regras_ordem = [
-    {
-      'inicio': '(',
-      'fim': ')',
-    },
-    {
-      'inicio': '[',
-      'fim': ']',
-    },
-    {
-      'inicio': '{',
-      'fim': '}',
-    },
-  ];
 
   Expressao({required this.dado}) {
     this.ordem = stringToList(valor: this.dado);
@@ -174,11 +222,11 @@ class Expressao {
   }
 
   segmentar({required List exp}) {
-    List total = exp;
-    List parte = [];
     bool agrupar = false;
+
+    List parte = [];
     List aux = exp;
-    String out = '';
+    List total = exp;
     for (var o in regras_ordem) {
       aux = total;
       total = [];
@@ -212,7 +260,7 @@ class Expressao {
 
 void main(List<String> args) {
   /*
-    EXEMPLOS
+    SAMPLES
 
     {(2+5)*[(3+4)+(2^2)]}*3
     [2^(0.5)]
